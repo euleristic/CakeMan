582eff7f56d353894b395539e3e0b4acd3275308
euleristic <valter.ottenvang@gmail.com>
2021-10-22 23:08:55 +0200

    DogMakeOver

--- Funny Skeleton Shoot Game/Assets/Scripts/DogBehaviour.cs
+++ Funny Skeleton Shoot Game/Assets/Scripts/DogBehaviour.cs
@@ -10,18 +10,22 @@ public class DogBehaviour : MonoBehaviour
         Chasing
     }
     State currentState;
+    State toState;
 
     public int hp;
     [SerializeField] float walkSpeed;
     [SerializeField] float runSpeed;
     [SerializeField] float detectionRange;
     [SerializeField] float gravity;
+    [SerializeField] Sprite neutralSprite;
+    [SerializeField] Sprite angrySprite;
 
     [Header("Don't change me!")]
     [SerializeField] Sensor forward;
     [SerializeField] Sensor forwardDown;
     [SerializeField] Sensor down;
 
+    SpriteRenderer spriteRenderer;
     GameObject player;
     bool facingRight;
     float detectionSqr;
@@ -29,21 +33,39 @@ public class DogBehaviour : MonoBehaviour
 
     void Start()
     {
-        facingRight = false;
+        facingRight = true;
         player = GameObject.FindGameObjectWithTag("Player");
         if (player == null)
             Destroy(this);
         detectionSqr = detectionRange * detectionRange;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (toState != currentState)
+        {
+            currentState = toState;
+            switch (currentState)
+            {
+                case State.Walking:
+                    spriteRenderer.sprite = neutralSprite;
+                    break;
+                case State.Chasing:
+                    spriteRenderer.sprite = angrySprite;
+                    break;
+                default:
+                    //what
+                    break;
+            }
+        }
+
         //x
         Vector3 playerRelative = player.transform.position - transform.position;
         if (playerRelative.sqrMagnitude < detectionSqr)
         {
-            currentState = State.Chasing;
+            toState = State.Chasing;
             velocity.x = new Vector2(playerRelative.x, 0f).normalized.x * runSpeed;
             if ((velocity.x < 0f && facingRight ||
                  velocity.x > 0f && !facingRight) && Mathf.Abs(playerRelative.x) > .1f)
@@ -54,7 +76,7 @@ public class DogBehaviour : MonoBehaviour
         }
         else
         {
-            currentState = State.Walking;
+            toState = State.Walking;
             if (forward.Triggered() || !forwardDown.Triggered())
             {
                 facingRight = !facingRight;
